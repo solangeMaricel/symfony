e762acec001e4c86d7d3b4ebff57373f49a4c68e
solangeMaricel <sanderson@frsf.utn.edu.ar>
2024-05-16 13:54:45 -0300

    Completo hasta modulo 13 del tutorial

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        $starshipCount = 457;
+
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
+        //return new Response('Hola Solange!');
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships'=> $starshipCount,
+            'myShip' => $myShip,
+        ]);
+    }
+}
